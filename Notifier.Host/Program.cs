5eb16d74edd539c273d3767de785cf3abd12a777
klacs <klacs1200@hotmail.com>
2016-10-30 19:19:21 +0800

    new changelist

---
+++ Notifier.Host/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notifier.Host
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
