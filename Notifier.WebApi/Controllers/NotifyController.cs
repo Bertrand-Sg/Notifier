e97ebc8b8e3c8e963b4c6d9a85c84bdb90095d75
Bertrand Decoux <klacs1200@hotmail.com>
2016-11-29 15:57:16 +0800

    Add random msgs

---
+++ Notifier.WebApi/Controllers/NotifyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Notifier.WebApi.Model;
+
+namespace Notifier.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotifyController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Notification> Get()
+        {
+            return CreateNotifications();
+        }
+
+        private static IEnumerable<Notification> CreateNotifications()
+        {
+            int limit = new Random().Next(1, 10);
+            var notifs = new List<Notification>();
+            var ids = new Random(limit);
+
+            for (var i = 0; i < limit; i++)
+            {
+                var id = ids.Next();
+                var categoryId = id % Enum.GetValues(typeof(NotificationType)).Length;
+                notifs.Add(new Notification()
+                {
+                    Id = id,
+                    Message = $"Message #{id}",
+                    Category = (NotificationType) categoryId,
+                    Title = $"title #{id}"
+                });
+            }
+
+            return notifs;
+        }
+
+        [HttpGet("base")]
+        public IEnumerable<NotificationBase> GetBase()
+        {
+            return CreateNotifications().OfType<NotificationBase>();
+        }
+
+        [HttpGet("test")]
+        public IEnumerable<string> GetTest()
+        {
+            return new[] {"Test 1", "Test 2"};
+        }
+    }
+}
---
+++ Notifier.WebApi/Model/Notification.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace Notifier.WebApi.Model
+{
+    [DataContract]
+    public class NotificationBase
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+    }
+
+    [DataContract]
+    public class Notification : NotificationBase
+    {
+        [DataMember]
+        public NotificationType Category { get; set; }
+    }
+
+    [DataContract]
+    public enum NotificationType
+    {
+        [EnumMember] None,
+        [EnumMember] Standard,
+        [EnumMember] Urgent,
+    }
+}
